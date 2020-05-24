ada0a5a4a14c520340b97073b6eab35fde5c9d80
rui.ishikawa <rui.ishikawa.business@gmail.com>
2020-05-24 16:47:38 +0900

    [modify] 起動コマンド修正

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.use(express.static('./dist/angular-heroku'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
+const path = require('path');
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
